DamjanJovanovic/epm

commit 8475324e0836a915463984fdbd8e9a6f090a1593

Enable AOO modifications

bsd.c
@@ -26,6 +26,18 @@
  * Local functions...
  */
 
+#ifdef __FOR_AOO__
+static void cr2semicolon(char *command)
+{
+  int		len, i;
+
+  len = strlen(command);
+  for (i = 0; i < len; i++)
+    if (*(command+i)=='\n')
+      *(command+i)=';';
+}
+#endif
+
 static int	make_subpackage(const char *prodname, const char *directory,
 		                const char *platname, dist_t *dist,
 			        const char *subpackage);
@@ -197,12 +209,24 @@ make_subpackage(
     if (d->subpackage != subpackage)
       continue;
 
+#ifdef __FOR_AOO__
+#ifdef __FreeBSD__
+    if (d->type == DEPEND_REQUIRES) {
+      if (dist->relnumber) {
+        fprintf(fp, "@pkgdep %s-%s-%d-%s", d->product, dist->version, dist->relnumber, platname);
+      } else {
+        fprintf(fp, "@pkgdep %s-%s-%s", d->product, dist->version, platname);
+      }
+    } else
+#endif /* __FreeBSD__ */
+#else
     if (d->type == DEPEND_REQUIRES)
 #ifdef __OpenBSD__
       fprintf(fp, "@depend %s", d->product);
 #else
       fprintf(fp, "@pkgdep %s", d->product);
 #endif /* __OpenBSD__ */
+#endif /* __FOR_AOO__ */
     else
 #ifdef __FreeBSD__
      /*
@@ -229,9 +253,15 @@ make_subpackage(
 	          "         by the BSD packager.\n", stderr);
             break;
 	case COMMAND_POST_INSTALL :
+#ifdef __FOR_AOO__
+	    cr2semicolon(c->command);
+#endif /* __FOR_AOO__ */
             fprintf(fp, "@exec %s\n", c->command);
 	    break;
 	case COMMAND_PRE_REMOVE :
+#ifdef __FOR_AOO__
+	    cr2semicolon(c->command);
+#endif /* __FOR_AOO__ */
             fprintf(fp, "@unexec %s\n", c->command);
 	    break;
 	case COMMAND_POST_REMOVE :

qprintf.c
@@ -173,12 +173,15 @@ qprintf(FILE       *fp,		/* I - File to write to */
 
             for (i = slen; i > 0; i --, s ++, bytes ++)
 	    {
+#if defined(__FreeBSD__) && defined(__FOR_AOO__)
+	      if (strchr("`~!#%^&*()[{]}\\|;\'\"<>? ", *s))
+#else
 	      if (strchr("`~#$%^&*()[{]}\\|;\'\"<>? ", *s))
+#endif
 	      {
 	        putc('\\', fp);
 		bytes ++;
 	      }
-
 	      putc(*s, fp);
 	    }
 

configure
@@ -710,6 +710,8 @@ with_archflags
 with_optim
 enable_debug
 enable_gui
+enable_fltk
+enable_aoo
 with_docdir
 with_softwaredir
 '
@@ -1336,6 +1338,8 @@ Optional Features:
   --enable-FEATURE[=ARG]  include FEATURE [ARG=yes]
   --enable-debug          turn on debugging [default=no]
   --enable-gui            turn on GUI utilities [default=auto]
+  --enable-fltk          enable fltk [default=auto]
+  --enable-aoo           enable AOO modifications [default=no]
 
 Optional Packages:
   --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
@@ -2241,6 +2245,18 @@ fi
 # Check whether --enable-gui was given.
 if test "${enable_gui+set}" = set; then :
   enableval=$enable_gui;
+fi
+
+
+# Check whether --enable-fltk was given.
+if test "${enable_fltk+set}" = set; then :
+  enableval=$enable_fltk;
+fi
+
+
+# Check whether --enable-aoo was given.
+if test "${enable_aoo+set}" = set; then :
+  enableval=$enable_aoo;
 fi
 
 
@@ -4612,7 +4628,11 @@ if test "$ac_res" != no; then :
 fi
 
 
-if test "x$enable_gui" != xno; then
+if test "x$enable_aoo" = xyes; then
+	CFLAGS="-D__FOR_AOO__ $CFLAGS"
+fi
+
+if test "x$enable_gui" != xno && test "x$enable_fltk" = xyes; then
 	# Extract the first word of "fltk-config", so it can be a program name with args.
 set dummy fltk-config; ac_word=$2
 { $as_echo "$as_me:${as_lineno-$LINENO}: checking for $ac_word" >&5

configure.ac
@@ -62,6 +62,10 @@ fi)
 
 AC_ARG_ENABLE(gui, [  --enable-gui            turn on GUI utilities [[default=auto]]])
 
+AC_ARG_ENABLE(fltk, [  --enable-fltk          enable fltk [[default=auto]]])
+
+AC_ARG_ENABLE(aoo, [  --enable-aoo           enable AOO modifications [[default=no]]])
+
 AC_ARG_WITH(docdir, [  --with-docdir=/dir      set directory for documentation
                           [[default=${datadir}/doc/epm]]],
 	docdir="$withval",
@@ -129,7 +133,11 @@ else
 fi
 AC_SEARCH_LIBS(gethostname, socket)
 
-if test "x$enable_gui" != xno; then
+if test "x$enable_aoo" = xyes; then
+	CFLAGS="-D__FOR_AOO__ $CFLAGS"
+fi
+
+if test "x$enable_gui" != xno && test "x$enable_fltk" = xyes; then
 	AC_PATH_PROG(FLTKCONFIG,fltk-config)
 else
 	FLTKCONFIG=""

deb.c
@@ -26,6 +26,31 @@
  * Local functions...
  */
 
+#ifdef __FOR_AOO__
+/*
+ * 'add_size()' - Append Installed-Size tag to DEBIAN/control file
+ */
+
+static int                      /* O - 0 = success, 1 = fail */
+add_size(FILE       *fpControl, /* Control file stream */
+         const char *directory) /* Directory containing all files to package */
+{
+    FILE *fp;
+    char command[1024];
+
+    snprintf(command, sizeof(command), "du -k -s %s", directory);
+    fp = popen(command, "r");
+    if( NULL != fp )
+    {
+        char size[1024];
+        fscanf(fp, "%s .", size);
+        fprintf(fpControl, "Installed-Size: %s\n", size);
+        return pclose(fp);
+    }
+    return 1;
+}
+#endif /* __FOR_AOO__ */
+
 static int	make_subpackage(const char *prodname, const char *directory,
 		                const char *platname, dist_t *dist,
 		                struct utsname *platform,
@@ -48,10 +73,33 @@ make_deb(const char     *prodname,	/* I - Product short name */
   char		name[1024],		/* Full product name */
 		filename[1024];		/* File to archive */
 
+#ifdef __FOR_AOO__
+  /*
+   * Use debian default naming scheme
+   */
+
+  if (!strcmp(platname, "intel"))
+#ifdef __FreeBSD_kernel__
+    platname = "kfreebsd-i386";
+#else
+    platname = "i386";
+#endif
+  else if (!strcmp(platname, "x86_64"))
+#ifdef __FreeBSD_kernel__
+    platname = "kfreebsd-amd64";
+#else
+    platname = "amd64";
+#endif
+  else if (!strcmp(platname, "ppc"))
+    platname = "powerpc";
+
+#else /* __FOR_AOO__ */
 
  /* Debian packages use "amd64" instead of "x86_64" for the architecture... */
   if (!strcmp(platname, "x86_64"))
     platname = "amd64";
+
+#endif /* __FOR_AOO__ */
 
   if (make_subpackage(prodname, directory, platname, dist, platform, NULL))
     return (1);
@@ -72,14 +120,26 @@ make_deb(const char     *prodname,	/* I - Product short name */
     */
 
     if (dist->release[0])
+#ifdef __FOR_AOO__
+      snprintf(name, sizeof(name), "%s_%s-%s", prodname, dist->version,
+#else /* __FOR_AOO__ */
       snprintf(name, sizeof(name), "%s-%s-%s", prodname, dist->version,
+#endif /* __FOR_AOO__ */
                dist->release);
     else
+#ifdef __FOR_AOO__
+      snprintf(name, sizeof(name), "%s_%s", prodname, dist->version);
+#else /* __FOR_AOO__ */
       snprintf(name, sizeof(name), "%s-%s", prodname, dist->version);
+#endif /* __FOR_AOO__ */
 
     if (platname[0])
     {
+#ifdef __FOR_AOO__
+      strlcat(name, "_", sizeof(name));
+#else /* __FOR_AOO__ */
       strlcat(name, "-", sizeof(name));
+#endif /* __FOR_AOO__ */
       strlcat(name, platname, sizeof(name));
     }
 
@@ -183,7 +243,11 @@ make_subpackage(const char     *prodname,
   */
 
   if (subpackage)
+#ifdef __FOR_AOO__
+    snprintf(prodfull, sizeof(prodfull), "%s_%s", prodname, subpackage);
+#else /* __FOR_AOO__ */
     snprintf(prodfull, sizeof(prodfull), "%s-%s", prodname, subpackage);
+#endif /* __FOR_AOO__ */
   else
     strlcpy(prodfull, prodname, sizeof(prodfull));
 
@@ -192,14 +256,26 @@ make_subpackage(const char     *prodname,
   */
 
   if (dist->release[0])
+#ifdef __FOR_AOO__
+    snprintf(name, sizeof(name), "%s_%s-%s", prodfull, dist->version,
+#else /* __FOR_AOO__ */
     snprintf(name, sizeof(name), "%s-%s-%s", prodfull, dist->version,
+#endif /* __FOR_AOO__ */
              dist->release);
   else
+#ifdef __FOR_AOO__
+    snprintf(name, sizeof(name), "%s_%s", prodfull, dist->version);
+#else /* __FOR_AOO__ */
     snprintf(name, sizeof(name), "%s-%s", prodfull, dist->version);
+#endif /* __FOR_AOO__ */
 
   if (platname[0])
   {
+#ifdef __FOR_AOO__
+    strlcat(name, "_", sizeof(name));
+#else /* __FOR_AOO__ */
     strlcat(name, "-", sizeof(name));
+#endif /* __FOR_AOO__ */
     strlcat(name, platname, sizeof(name));
   }
 
@@ -239,6 +315,24 @@ make_subpackage(const char     *prodname,
   * (which we change in get_platform to a common name)
   */
 
+#ifdef __FOR_AOO__
+  if (!strcmp(platform->machine, "intel"))
+#ifdef __FreeBSD_kernel__
+    fputs("Architecture: kfreebsd-i386\n", fp);
+#else
+    fputs("Architecture: i386\n", fp);
+#endif
+  else if (!strcmp(platform->machine, "x86_64"))
+#ifdef __FreeBSD_kernel__
+    fputs("Architecture: kfreebsd-amd64\n", fp);
+#else
+    fputs("Architecture: amd64\n", fp);
+#endif
+  else if (!strcmp(platform->machine, "ppc"))
+    fputs("Architecture: powerpc\n", fp);
+  else
+    fprintf(fp, "Architecture: %s\n", platform->machine);
+#else /* __FOR_AOO__ */
   if (!strcmp(platform->machine, "intel"))
     fputs("Architecture: i386\n", fp);
   if (!strcmp(platform->machine, "x86_64"))
@@ -247,6 +341,7 @@ make_subpackage(const char     *prodname,
     fputs("Architecture: powerpc\n", fp);
   else
     fprintf(fp, "Architecture: %s\n", platform->machine);
+#endif
 
   fprintf(fp, "Description: %s\n", dist->product);
   fprintf(fp, " Copyright: %s\n", dist->copyright);
@@ -503,6 +598,27 @@ make_subpackage(const char     *prodname,
       fprintf(fp, "/etc/init.d/%s\n", file->dst);
 
   fclose(fp);
+
+#ifdef __FOR_AOO__
+  /*
+   * Calculate and append Installed-Size to DEBIAN/control
+   */
+
+  if (Verbosity)
+    puts("Calculating Installed-Size...");
+
+  snprintf(filename, sizeof(filename), "%s/%s/DEBIAN/control", directory, name);
+  if ((fp = fopen(filename, "a")) == NULL)
+  {
+    fprintf(stderr, "epm: Unable to Installed-Size to file \"%s\" - %s\n", filename,
+            strerror(errno));
+    return (1);
+  }
+
+  snprintf(filename, sizeof(filename), "%s/%s", directory, name);
+  add_size(fp, filename);
+  fclose(fp);
+#endif /* __FOR_AOO__ */
 
  /*
   * Copy the files over...

epm.c
@@ -649,6 +649,9 @@ info(void)
 {
   puts(EPM_VERSION);
   puts("Copyright 1999-2017 by Michael R Sweet.");
+#ifdef __FOR_AOO__
+  puts("Patched for Apache OpenOffice.org");
+#endif /* __FOR_AOO__ */
   puts("");
   puts("EPM is free software and comes with ABSOLUTELY NO WARRANTY; for details");
   puts("see the GNU General Public License in the file COPYING or at");

file.c
@@ -98,8 +98,10 @@ copy_file(const char *dst,		/* I - Destination file */
 
   if (mode)
     chmod(dst, mode);
+#ifndef __FOR_AOO__
   if (owner != (uid_t)-1 && group != (gid_t)-1)
     chown(dst, owner, group);
+#endif /* __FOR_AOO__ */
 
   return (0);
 }
@@ -130,8 +132,10 @@ make_directory(const char *directory,	/* I - Directory */
 	mkdir(buffer, 0755);
 	if (mode)
           chmod(buffer, mode | 0700);
+#ifndef __FOR_AOO__
 	if (owner != (uid_t)-1 && group != (gid_t)-1)
 	  chown(buffer, owner, group);
+#endif /* __FOR_AOO__ */
       }
     }
 
@@ -145,8 +149,10 @@ make_directory(const char *directory,	/* I - Directory */
     mkdir(buffer, 0755);
     if (mode)
       chmod(buffer, mode | 0700);
+#ifndef __FOR_AOO__
     if (owner != (uid_t)-1 && group != (gid_t)-1)
       chown(buffer, owner, group);
+#endif /* __FOR_AOO__ */
   }
 
   return (0);

pkg.c
@@ -465,6 +465,7 @@ make_pkg(const char     *prodname,	/* I - Product short name */
 
   fclose(fp);
 
+#ifndef __FOR_AOO__
  /*
   * Build the distribution from the prototype file...
   */
@@ -548,7 +549,7 @@ make_pkg(const char     *prodname,	/* I - Product short name */
     snprintf(filename, sizeof(filename), "%s/%s", directory, prodname);
     unlink_directory(filename);
   }
-
+#endif /* __FOR_AOO__ */
   return (0);
 }
 

commit b4a825db57d790667a850495745d50bb8a1f0f6f

FreeBSD fixes for a build regression, introduced in 8475324e0836a915463984fdbd8e9a6f090a1593. There is no "relnumber", use "vernumber" instead. Include "epm.h" for AooMode in qprintf.c.

bsd.c
@@ -200,9 +200,9 @@ make_subpackage(const char *prodname,   /* I - Product short name */
         if (d->type == DEPEND_REQUIRES) {
 #ifdef __FreeBSD__
             if (AooMode) {
-                if (dist->relnumber) {
+                if (dist->vernumber) {
                     fprintf(fp, "@pkgdep %s-%s-%d-%s", d->product, dist->version,
-                            dist->relnumber, platname);
+                            dist->vernumber, platname);
                 } else {
                     fprintf(fp, "@pkgdep %s-%s-%s", d->product, dist->version, platname);
                 }

qprintf.c
@@ -22,6 +22,7 @@
  * Include necessary headers...
  */
 
+#include "epm.h"
 #include "epmstring.h"
 #include <ctype.h>
 #include <stdio.h>